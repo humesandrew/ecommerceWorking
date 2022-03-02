243b825bd714ed4d7e56b36d53a47c4d3be48744
Andy Humes <andrew.humes@du.edu>
2022-03-02 14:11:12 -0700

    finished routes for products and tags

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const {
+  Category,
+  Product
+} = require('../../models');
 
 // The `/api/categories` endpoint
 
@@ -7,8 +10,8 @@ router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    include: [Product],
-  })
+      include: [Product],
+    })
     .then((categories) => {
       res.status(200).json(categories);
     })
@@ -29,42 +32,38 @@ router.get("/:id", (req, res) => {
 });
 
 
-  router.post("/", (req, res) => {
-    // create a new category
-    Category.create(req.body)
-      .then((category) => {
-        res.status(200).json(category);
-      })
-      .catch((err) => {
-        res.status(500).json(err);
-      });
-  });
+router.post("/", (req, res) => {
+  // create a new category
+  Category.create(req.body)
+    .then((category) => {
+      res.status(200).json(category);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
-  router.put("/:id", async (req, res) => {
-    const categoryUpdateId = req.params.id;
-    const resultUpdate = await Category.update(
-      {
-        category_name: req.body.category_name,
-      },
-      {
-        where: {
-          id: categoryUpdateId,
-        },
-      }
-    );
-    res.json(resultUpdate);
+router.put("/:id", async (req, res) => {
+  const categoryUpdateId = req.params.id;
+  const resultUpdate = await Category.update({
+    category_name: req.body.category_name,
+  }, {
+    where: {
+      id: categoryUpdateId,
+    },
   });
+  res.json(resultUpdate);
+});
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   const categoryDeleteId = req.params.id;
-  const resultDelete = await Category.destroy(
-    {
-      where: { 
-        id: categoryDeleteId,
-      },
-    }
-  ); res.json(resultDelete);
+  const resultDelete = await Category.destroy({
+    where: {
+      id: categoryDeleteId,
+    },
+  });
+  res.json(resultDelete);
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,76 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const {
+  Tag,
+  Product,
+  ProductTag
+} = require('../../models');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+      include: [Product]
+    })
+    .then((categories) => {
+      res.status(200).json(categories);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-});
+      // find a single tag by its `id`
+      // be sure to include its associated Product data
+      Tag.findByPk(req.params.id)
+        .then((tags) => {
+          res.status(200).json(tags);
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    });
 
-router.post('/', (req, res) => {
-  // create a new tag
-});
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
-});
 
-module.exports = router;
+      router.post('/', (req, res) => {
+        // create a new tag
+        Tag.create(req.body)
+        .then((tags) => {
+          res.status(200).json(tags);
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    });
+
+      router.put('/:id', (req, res) => {
+        Tag.update(req.body, {
+          where: {
+            id: req.params.id,
+          },
+        })
+        .then((tags) => {
+          res.status(200).json(tags);
+        })
+        .catch(err => {
+          res.status(500).json(err);
+        })
+        // update a tag's name by its `id` value
+      });
+
+      router.delete('/:id', async (req, res) => {
+        // delete on tag by its `id` value
+          const tagDeleteId = req.params.id;
+          const resultDelete = await Tag.destroy({
+            where: {
+              id: tagDeleteId,
+            },
+          });
+          res.json(resultDelete);
+        });
+
+      module.exports = router;
